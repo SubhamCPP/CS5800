12920c9bd4656f6c3f64d1dcf86231c23633fcc9
SubhamCPP <150965859+SubhamCPP@users.noreply.github.com>
2024-05-12 15:22:34 -0700

    Delete FinalProject directory

--- FinalProject/Project01/project01/src/main/java/cppfooddelivery/users/Customer.java
+++
@@ -1,7 +0,0 @@
-package cppfooddelivery.users;
-
-public class Customer extends User {
-    public Customer(String name, String address, String county) {
-        super(name, address, county);
-    }
-}
